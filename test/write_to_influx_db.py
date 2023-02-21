37bdc88d76cd43d154ef241fb9f93a75cfddf395
Vojtěch Petrásek <vojtechpetrasek@gmail.com>
2023-02-21 22:27:55 +0000

    added final test app for sending data to influxdb

--- test/write_to_influx_db.py
+++ test/write_to_influx_db.py
@@ -14,13 +14,38 @@
 
 import sys
 import time
+from influxdb_client import InfluxDBClient, Point
+from influxdb_client.client.write_api import SYNCHRONOUS
 
 ###
 # main
 ###
 
 def main():
-    print("hello world")
+    token = ""
+    with open("secret_token.txt", "r") as t:
+        token = t.readline().rstrip()
+        
+    bucket = "d70c4c066310c72e"
+    org = "uu team 8"
+    
+    client = InfluxDBClient(url="https://influx.uu.vojtechpetrasek.com", token=token, org=org)
+    print(client)
+    write_api = client.write_api(write_options=SYNCHRONOUS)
+    query_api = client.query_api()
+    p = Point("my_measurement").tag("location", "Prague").field("temperature", 25.3)
+    write_api.write(bucket=bucket, record=p)
+    query = ' from(bucket:"test")\
+            |> range(start: -10m)\
+            |> filter(fn:(r) => r._measurement == "my_measurement")'
+    #        |> filter(fn: (r) => r.location == "coyote_creek")\
+    #        |> filter(fn:(r) => r._field == "water_level" )
+    tables = query_api.query(query)
+
+    for table in tables:
+        print(table)
+        for row in table.records:
+            print (row.values)
     
 
 if __name__ == '__main__':
