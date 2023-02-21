ea1abd7f4c6ff5a90ef989152937fa74a394bbf3
Vojtěch Petrásek <vojtechpetrasek@gmail.com>
2023-02-21 21:36:59 +0000

    add influxdb test file

---
+++ test/write_to_influx_db.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+###################################################
+# Conector to db
+# Title: Connector which sends data to influxdb
+# Author: Vojtěch Petrásek
+# Generated: 21/02/2023 22:22:22
+###################################################
+
+###
+# imports
+###
+
+import sys
+import time
+
+###
+# main
+###
+
+def main():
+    print("hello world")
+    
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as e:
+        print(e)
+        sys.exit()
