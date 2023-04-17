28c3e13183dd0ca965e90f88d499c7ae2653fba8
Vojtěch Novotný <vojta.nvtn@gmail.com>
2023-04-17 22:18:53 +0200

    Smazání develop z masteru

--- README.md
+++ README.md
@@ -1,43 +1,8 @@
 # uuCloud
 
-## Install python packages
-```
-pip3 install -r requirements.txt
-```
-
-## Start API
-```
-python3 api.py
-```
-
-
 ## API documentation
 https://api.uu.vojtechpetrasek.com/swagger/
 
 ## API 
 
 > runs on port 8000
-
-start api service
-```
-systemctl start api.service
-```
-stop api service
-```
-systemctl stop api.service
-```
-restart api service
-```
-systemctl restart api.service
-```
-
-## InfluxDB
-> internaly runs on port 8086
-https://influx.uu.vojtechpetrasek.com/
-
-## Grafana
-> internaly runs on port 3000
-https://grafana.uu.vojtechpetrasek.com/
-
-### Don't look here
-stealing code: https://github.com/Sean-Bradley/Seans-Python3-Flask-Rest-Boilerplate
--- api.py
+++ api.py
@@ -10,16 +10,9 @@
 
 from datetime import datetime
 import sys
-import json
 
 from flask import Flask, request
 from flask_swagger_ui import get_swaggerui_blueprint
-from influxdb_client import InfluxDBClient, Point
-from influxdb_client.client.write_api import SYNCHRONOUS
-
-###
-# flask setup
-###
 
 app = Flask(__name__)
 
@@ -35,39 +28,14 @@ SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
 )
 app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
 
-###
-# influxdb setup
-###
-
-token = ""
-with open("test/secret_token.txt", "r") as t:
-    token = t.readline().rstrip()
-bucket = "test"
-org = "uu team 8"
-client = InfluxDBClient(url="https://influx.uu.vojtechpetrasek.com", token=token, org=org)
-write_api = client.write_api(write_options=SYNCHRONOUS)
-
 @app.route("/")
 def home():
     return "Welcome to api"
 
 @app.route("/v1/sensor", methods=["POST"])
 def sensor():
-    raw_data = request.data.decode('utf-8').rstrip().split(' ')
-    p = Point("my_measurement").tag("location", "Prague").field(raw_data[0], float(raw_data[2]))
-    write_api.write(bucket=bucket, record=p)
+    raw_data = request.data.decode('utf-8').rstrip()
     print(raw_data)
-    return "ok"
-
-@app.route("/v1/solar", methods=["POST"])
-def solar():
-    data = json.loads(request.json)
-    for i in data:
-        try:
-            p = Point("fve_home").tag("location", "Varnsdorf").field(i, float(data[i][0]))
-            write_api.write(bucket="fve", record=p)
-        except Exception as e:
-            print(e)
     return "ok"
 
 if __name__ == '__main__':
