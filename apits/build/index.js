3455ca735f223e44bced994474b1503a219acb0a
Vojtěch Novotný <vojta.nvtn@gmail.com>
2023-05-20 18:12:24 +0200

    Backend

--- apits/build/index.js
+++ apits/build/index.js
@@ -1,2 +1,15 @@
 "use strict";
-console.log("fufaif");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+console.log(process.env);
+const app = (0, express_1.default)();
+const port = process.env.PORT;
+console.log(port);
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
--- apits/src/index.ts
+++ apits/src/index.ts
@@ -1,1 +1,14 @@
-console.log("fufaif");
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+console.log(process.env);
+const app = express();
+const port = process.env.PORT;
+
+console.log(port);
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
