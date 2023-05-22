22b1a6f51e94b5edf5c8d5fef3e22664f9a2a53e
Vojtěch Novotný <vojta.nvtn@gmail.com>
2023-05-22 16:36:52 +0200

    Backend

--- api/src/routes/temperature.ts
+++ api/src/routes/temperature.ts
@@ -2,8 +2,7 @@ import { Request, Response } from "express";
 import { influxDB } from "../databases";
 
 export async function getTemperature(req: Request, res: Response) {
-    const fluxQuery =
-        'from(bucket:"test") |> range(start: 0) |> filter(fn: (r) => r._measurement == "my_measurement") |> filter(fn: (r) => r._field == "temperature") |> last()'
+    const fluxQuery = 'from(bucket:"test") |> range(start: 0) |> filter(fn: (r) => r._measurement == "my_measurement") |> filter(fn: (r) => r._field == "temperature") |> last()'
 
     const myQuery = async () => {
         for await (const { values, tableMeta } of influxDB.iterateRows(fluxQuery)) {
