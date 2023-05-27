27fdf7d9a2d335fdcd36878dfbec86f2ededf956
Vojtěch Novotný <vojta.nvtn@gmail.com>
2023-05-27 13:12:40 +0200

    Backend

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,13 +1,13 @@
 import { Router } from "express";
 
 import { register, login, logout } from "./routes/auth";
-import { getGatewayData } from "./routes/gateway";
+import { getGateway } from "./routes/gateway";
 
 const router = Router();
 
 router.post("/register", register);
 router.post("/login", login);
 router.post("/logout", logout);
-router.post("/gateway", getGatewayData);
+router.post("/gateway", getGateway);
 
 export default router;
