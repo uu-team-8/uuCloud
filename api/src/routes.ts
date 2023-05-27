1808e9c31c9d21d2ad5abe1f9d3b8e1adca248d4
Vojtěch Novotný <vojta.nvtn@gmail.com>
2023-05-27 13:46:02 +0200

    Backend

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 
 import { register, login, logout } from "./routes/auth";
-import { getGateway } from "./routes/gateway";
+import { getGateway, editGateway, deleteGateway } from "./routes/gateway";
 
 const router = Router();
 
@@ -9,5 +9,7 @@ router.post("/register", register);
 router.post("/login", login);
 router.post("/logout", logout);
 router.post("/gateway", getGateway);
+router.post("/gateway/edit", editGateway);
+router.post("/gateway/delete/:id", deleteGateway);
 
 export default router;
--- api/src/routes/gateway.ts
+++ api/src/routes/gateway.ts
@@ -1,5 +1,40 @@
 import type { Request, Response } from "express";
+import { mysqlDB } from "../databases";
 
 export async function getGateway(req: Request, res: Response) {
 
+}
+
+interface EditGatewayReq {
+    id: number
+    name: string
+    secret: string
+    ip: string
+    owner: number
+}
+
+export async function editGateway(req: Request, res: Response) {
+    const data: EditGatewayReq = req.body;
+
+    try {
+        await mysqlDB.execute("UPDATE gateway SET name = ? WHERE id = ?", [data.name, data.id]);
+    } catch (e) {
+        console.error(e);
+        res.json({ success: false });
+    }
+
+    res.json({ success: true, message: "Změna dat proběhla úspěšně" });
+}
+
+export async function deleteGateway(req: Request, res: Response) {
+    const id = req.params.id;
+
+    try {
+        await mysqlDB.execute("DELETE FROM gateway WHERE id = ?", [id]);
+    } catch (e) {
+        console.error(e);
+        res.json({ success: false });
+    }
+
+    res.json({ success: true });
 }
